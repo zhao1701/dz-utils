19c31e26b4848fe524d3475940ab949b01903d5c
Derek Zhao <chong.zhao@columbia.edu>
2019-03-19 22:13:44 -0700

    first commit

---
+++ dzu/debug.py
@@ -0,0 +1,32 @@
+import functools as ft
+
+
+def trace(func):
+    @ft.wraps(func)
+    def wrapper(*args, **kwargs):
+        print(
+            'TRACE: Calling {}() with position arguments {} and '
+            'keyword arguments {}'.format(
+                func.__name__, args, kwargs))
+        result = func(*args, **kwargs)
+        print(
+            'TRACE: {}() returned {}'.format(
+                func.__name__, result))
+        return result
+    return wrapper
+
+
+class show_status:
+
+    def __init__(self, message):
+        self.message = message
+
+    def __call__(self, func):
+        
+        @ft.wraps(func)
+        def wrapper(*args, **kwargs):
+            print('{} ... '.format(self.message), end='')
+            result = func(*args, **kwargs)
+            print('success.')
+            return result
+        return wrapper
---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='dz-utils',
+    version='0.0.1',
+    description='Derek\'s commonly used helper functions',
+    url='http://github.com/zhao1701/dz-utils',
+    author='Derek Zhao',
+    author_email='chong.zhao@columbia.edu',
+    license='MIT',
+    packages=setuptools.find_packages(),
+    zip_safe=False)
